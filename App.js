0783fc20139ad0f8c8f35b4bc6c340c475574127
furqanalishah <syedfurqan@wanclouds.net>
2022-09-15 14:45:39 +0500

    use Dashboard Screen in App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, StatusBar as RStatusBar } from "react-native";
+import Dashboard from "./src/screens/Dashboard";
+import { colors } from "./src/utils/colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Dashboard />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.offWhite,
+    marginTop: RStatusBar.currentHeight,
   },
 });

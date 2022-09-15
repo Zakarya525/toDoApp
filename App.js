bb195d35dc9f96de068f1e9fc0ad16bdd42195ae
furqanalishah <syedfurqan@wanclouds.net>
2022-09-16 00:34:36 +0500

    start using Google Roboto font; import it in App.js

--- App.js
+++ App.js
@@ -1,9 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, StatusBar as RStatusBar } from "react-native";
+import { StatusBar as RStatusBar, StyleSheet, View } from "react-native";
 import Dashboard from "./src/screens/Dashboard";
 import { colors } from "./src/utils/colors";
+import {
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Dashboard />

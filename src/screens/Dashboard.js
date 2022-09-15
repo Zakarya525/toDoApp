636d0141bee730f183ec80caa52520968cffe59c
furqanalishah <syedfurqan@wanclouds.net>
2022-09-16 00:32:43 +0500

    remove hardcoded spacings and sizings

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,11 +1,14 @@
 import { StyleSheet, View } from "react-native";
 import Header from "../components/Header";
 import { colors } from "../utils/colors";
+import Body from "../components/Body";
+import { spacing } from "../utils/sizes";
 
 const Dashboard = () => {
   return (
     <View style={styles.dashboard}>
       <Header />
+      <Body />
     </View>
   );
 };
@@ -14,13 +17,13 @@ export default Dashboard;
 
 const styles = StyleSheet.create({
   dashboard: {
-    flex: 0.4,
+    flex: 1,
     backgroundColor: colors.lightOrange,
   },
   text: {
     color: "white",
-    fontSize: 42,
-    lineHeight: 84,
+    fontSize: spacing.xxl + 2,
+    lineHeight: spacing.xxxl + 4,
     fontWeight: "bold",
     textAlign: "center",
   },

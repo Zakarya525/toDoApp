e5b3c14ca6f303d0566eec45da6b7aa5be02902c
furqanalishah <syedfurqan@wanclouds.net>
2022-09-19 01:44:03 +0500

    fix login

--- src/api/user_api.js
+++ src/api/user_api.js
@@ -1,20 +1,15 @@
 import ApiManager from './ApiManager'
 
-export default user_api = async data => {
-  console.warn(data);
-  try {
-    const result = await ApiManager.post('/login', {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: JSON.stringify(
-        `grant_type=&username=${data.username}&password=${data.password}&scope=&client_id=&client_secret=`,
-      ),
-
-    })
-    console.log(result);
-    return result
-  } catch (error) {
-    console.log(error)  
-  }
+export const user_api = async data => {
+    try {
+        return await ApiManager('/login', {
+            method: 'POST',
+            headers: {
+                ContentType: 'application/x-www-form-urlencoded'
+            },
+            data: JSON.stringify(`grant_type=&username=${data.username}&password=${data.password}&scope=&client_id=&client_secret=`,),
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
 }
